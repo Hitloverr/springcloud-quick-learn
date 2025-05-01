d89bb98276ad7eaf6c299948c5c06275e36076c6
xiangxiao <244847243@qq.com>
2025-05-01 19:44:37 +0800

    Nacos：配置监听

--- code/services/service-order/src/main/java/org/example/OrderMainApplication.java
+++ code/services/service-order/src/main/java/org/example/OrderMainApplication.java
@@ -1,8 +1,17 @@
 package org.example;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +21,22 @@ public class OrderMainApplication {
         SpringApplication.run(OrderMainApplication.class, args);
     }
 
+    // 项目启动就监听配置文件变化。
+    @Bean
+    ApplicationRunner applicationRunner(NacosConfigManager nacosConfigManager) {
+        return args -> {
+            ConfigService configService = nacosConfigManager.getConfigService();
+            configService.addListener("service-order.properties", "DEFAULT_GROUP", new Listener() {
+                @Override
+                public Executor getExecutor() {
+                    return Executors.newFixedThreadPool(2);
+                }
+
+                @Override
+                public void receiveConfigInfo(String s) {
+                    System.out.println("接收到的配置信息" + s);
+                }
+            });
+        };
+    }
 }
