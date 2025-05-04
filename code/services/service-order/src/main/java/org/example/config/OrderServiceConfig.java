ed6a4cc01034859b7dc7c239da722ee3e706a71d
xiangxiao <~>
2025-05-04 14:56:42 +0800

    Feign: 详细日志

--- code/services/service-order/src/main/java/org/example/config/OrderServiceConfig.java
+++ code/services/service-order/src/main/java/org/example/config/OrderServiceConfig.java
@@ -1,5 +1,6 @@
 package org.example.config;
 
+import feign.Logger;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +13,10 @@ public class OrderServiceConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
